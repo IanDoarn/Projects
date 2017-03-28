8e62a7220a8cc6ce51d20e0e238840f73ac231fd
iandoarn <ian.doarrn@zimmerbiomet.com>
2017-03-28 11:01:44 -0500

    gitty stuff

--- C#/gitty/gitty/gitty/Program.cs
+++ C#/gitty/gitty/gitty/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,142 @@ namespace gitty
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            parseArgument(string.Join(" ", args));
+        }
+        private static void gitMain()
+        {
+            string gitExe = "";
+            string rootDirectory = Settings.Default.root;
+
+            if (Settings.Default.root == "none")
+            {
+                rootDirectory = Utils.askUser("User default root [" + Settings.Default.root + "]", "Enter root directory: ", @"C:\Users\");
+                bool saveRoot = Utils.askUser("Save [" + rootDirectory + "] as default root?", null, true);
+                if (saveRoot)
+                {
+                    Settings.Default.root = rootDirectory;
+                    Settings.Default.Save();
+                }
+
+            }
+
+            Console.WriteLine("Root directory at " + rootDirectory);
+            Console.WriteLine("Enter project directory: ");
+            string projectDirectory = Console.ReadLine();
+
+            Console.WriteLine("Enter commit note: ");
+            string commitNote = Console.ReadLine();
+
+            string pushType = Utils.askUser("Use force push?", "Enter push type: ", Settings.Default.pushCommand);
+
+            if(Settings.Default.gitexecutable == "none")
+            {
+                try
+                {
+                    gitExe = Environment.GetEnvironmentVariable("ProgramFiles") + "\\Git\\cmd\\git.exe";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("Please enter git.exe location: ");
+                    gitExe = Console.ReadLine();
+                    Settings.Default.gitexecutable = gitExe;
+                    Settings.Default.Save();
+                }
+            }
+
+            gitExe = Settings.Default.gitexecutable;
+
+            try
+            {
+                git(gitExe, addFiles());
+                git(gitExe, commit(commitNote));
+                git(gitExe, push(pushType));
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error occurred while running gitty");
+                Console.WriteLine(e.Message.ToString());
+            }
+            finally
+            {
+                close();
+            }
+
+            close();
+
+        }
+
+        private static void close()
+        {
+            Console.Write("Press any key to exit or close the window.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+        private static void git(string gitExecutableLocation ,string command)
+        {
+            string gitExecutable = gitExecutableLocation;
+
+            Process process = new Process();
+ 
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.FileName = gitExecutable;
+            process.StartInfo.Arguments = command;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private static string addFiles(string files=".")
+        {
+            if(!(files == "."))
+            {
+                string[] allFiles = files.Split(',');
+                files = string.Join(" ", allFiles);
+                return "git add " + files;
+            }
+            return "add " + files;
+        }
+
+        private static string commit(string commitNote="")
+        {
+            if(!(commitNote == ""))
+            {
+                return "commit -m \"\"";
+            }
+
+            return "commit -m \"" + commitNote + "\"";
+        }
+
+        private static string push(string pushType="-f")
+        {
+            if(!(pushType == "-f"))
+            {
+                return "push " + pushType;
+            }
+
+            return "push " + pushType;
+        }
+
+
+        private static void parseArgument(string arg)
+        {
+            if((arg == "-h" || arg == "--help"))
+            {
+                Console.WriteLine(Utils.Menu());
+                Environment.Exit(0);
+            }
+
+            else if (arg == "-u")
+            {
+                Utils.updateArg(arg);
+            }
         }
     }
 }
---
+++ C#/gitty/gitty/gitty/Utils.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitty
+{
+    class Utils
+    {
+        public static string Menu()
+        {
+            string menu = "Gitty usage:\n\n" +
+                          "-u\tUpdate settings\n" +
+                          "-g\tEnter Git\n" +
+                          "-e\tExit";        
+            return menu;
+        }
+
+        public static void updateSettingsItem(dynamic settingsItem, dynamic value)
+        {
+            settingsItem = value;
+            settingsItem.Save();
+        }
+
+        public static dynamic askUser(string message, string submessage, dynamic defaultReturn = null)
+        {
+            Console.WriteLine(message + " ? Y/N:");
+            string response = Console.ReadLine();
+
+            if (!(response.ToUpper() == "Y"))
+            {
+                if (submessage == null)
+                {
+                    return defaultReturn;
+                }
+
+                Console.WriteLine(submessage);
+                return Console.ReadLine();
+            }
+
+            return defaultReturn;
+        }
+
+        public static void updateArg(string arg)
+        {
+            if (!(arg.Length > 2))
+            {
+                Console.WriteLine("Must pass argurment of setting name:\n-u [SETTINGKEY] [VALUES...]");
+                Environment.Exit(0);
+            }
+            else
+            {
+                string[] args = arg.Split(' ');
+
+                if (args[2].Contains("help"))
+                {
+                    Console.WriteLine("Available Keys:\nhelp\nroot\ngit\nlogin");
+                }
+                else if (args[1] == "root")
+                {
+                    updateSettingsItem(Settings.Default.root, args[2]);
+                    Console.WriteLine("Root now at: " + Settings.Default.root);
+                }
+
+                else if (args[1] == "git")
+                {
+                    string gitExe = "";
+
+                    if (args[2] == null)
+                    {
+                        try
+                        {
+                            gitExe = Environment.GetEnvironmentVariable("ProgramFiles") + "\\Git\\cmd\\git.exe";
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message.ToString());
+                        }
+                        finally
+                        {
+                            Console.WriteLine("Please enter git.exe location: ");
+                            gitExe = Console.ReadLine();
+                            updateSettingsItem(Settings.Default.gitexecutable, gitExe);
+                        }
+                    }
+
+                    updateSettingsItem(Settings.Default.gitexecutable, args[2]);
+                    Environment.Exit(0);
+
+
+                }
+
+                else if (args[1] == "login")
+                {
+                    if (args.Length < 3)
+                    {
+                        Console.WriteLine("Invalid number of arguments for " + args[1] + ".\nRequired: [USER] [PASS]");
+                        Environment.Exit(0);
+                    }
+
+                    updateSettingsItem(Settings.Default.username, args[2]);
+                    updateSettingsItem(Settings.Default.username, args[3]);
+                }
+            }        
+        }
+    }
+}
